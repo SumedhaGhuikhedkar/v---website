3c7fb585e580c2212da6936660671099266274db
SumedhaGhuikhedkar <163756185+SumedhaGhuikhedkar@users.noreply.github.com>
2026-02-04 14:17:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function yesClicked() {
+    document.getElementById("message").innerHTML =
+        "Yayyy! 💕 I knew it! 😍";
+}
+
+function moveNo() {
+    let x = Math.random() * 200 - 100;
+    let y = Math.random() * 200 - 100;
+
+    document.getElementById("noBtn").style.transform =
+        `translate(${x}px, ${y}px)`;
+}
+
 
